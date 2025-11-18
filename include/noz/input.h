98ab0e81551cab5c7fe6d1c7a9f90428b4a09899
Bryan Dube <bdube@nozgames.com>
2025-11-18 08:07:10 -0600

    minor input addition

--- include/noz/input.h
+++ include/noz/input.h
@@ -42,6 +42,7 @@ inline bool IsShiftDown() { return IsButtonDown(KEY_LEFT_SHIFT) || IsButtonDown(
 inline bool IsShiftDown(InputSet* map) { return IsButtonDown(map, KEY_LEFT_SHIFT) || IsButtonDown(map, KEY_RIGHT_SHIFT); }
 inline bool IsAltDown(InputSet* map) { return IsButtonDown(map, KEY_LEFT_ALT) || IsButtonDown(map, KEY_RIGHT_ALT); }
 inline bool IsCtrlDown(InputSet* map) { return IsButtonDown(map, KEY_LEFT_CTRL) || IsButtonDown(map, KEY_RIGHT_CTRL); }
+inline bool IsCtrlDown() { return IsButtonDown(KEY_LEFT_CTRL) || IsButtonDown(KEY_RIGHT_CTRL); }
 extern bool WasButtonPressed(InputSet* map, InputCode code);
 inline bool WasButtonPressed(InputCode code) { return WasButtonPressed(GetInputSet(), code); }
 extern bool WasButtonReleased(InputSet* map, InputCode code);
